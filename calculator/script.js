10ca18e022c62f9d72d605d23be991833ac14446
mariyem-fatima <heymariyem@gmail.com>
2025-06-01 20:57:33 +0500

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,35 @@
+const display = document.getElementById('display');
+
+    function appendValue(val) {
+      display.value += val;
+    }
+
+    function clearDisplay() {
+      display.value = '';
+    }
+
+    function backspace() {
+      display.value = display.value.slice(0, -1);
+    }
+
+    function calculate() {
+      try {
+        display.value = eval(display.value);
+      } catch {
+        display.value = 'Error';
+      }
+    }
+
+    document.addEventListener('keydown', (e) => {
+      const key = e.key;
+      if (/[0-9+\-*/().]/.test(key)) {
+        appendValue(key);
+      } else if (key === 'Enter') {
+        e.preventDefault();
+        calculate();
+      } else if (key === 'Backspace') {
+        backspace();
+      } else if (key.toLowerCase() === 'c') {
+        clearDisplay();
+      }
+    });
